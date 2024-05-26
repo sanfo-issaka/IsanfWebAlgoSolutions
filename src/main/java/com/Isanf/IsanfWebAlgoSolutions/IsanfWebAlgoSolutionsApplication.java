4d5d9e6dccaf8b09fe18dac78deccc2a08112047
issaka-sanfo <issak.sanf@gmail.com>
2024-05-26 19:23:25 +0200

    init

---
+++ src/main/java/com/Isanf/IsanfWebAlgoSolutions/IsanfWebAlgoSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.Isanf.IsanfWebAlgoSolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsanfWebAlgoSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsanfWebAlgoSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Isanf/IsanfWebAlgoSolutions/IsanfWebAlgoSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Isanf.IsanfWebAlgoSolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsanfWebAlgoSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
